e8bb60d3806ac1a1118fdf8f42ff130c8e98fa56
Kyle <kylemit@gmail.om>
2014-06-19 01:44:17 -0400

    Give users the option to change viewport
    This helps illustrate a key way in which the viewport changes the way
    the page renders.
    
    This will have no affect on a desktop browsers because they ignore the
    viewport meta tag.
    Instead, to see the full effect, use the AMAZING chrome emulator:
    https://developer.chrome.com/devtools/docs/mobile-emulation
    https://www.youtube.com/watch?v=z7sTRdSpA04

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-﻿
+﻿$(function() {
+    $(".MobileToggle a").click(function () {
+        var viewport = ($(this).attr('id') === "MobileSite") ?
+                        'width=device-width, initial-scale=1.0' :
+                        'width=1000';
+        $("meta[name=viewport]").attr('content', viewport);
+        $(".MobileToggle").toggle();
+        return false;
+    });
+});
